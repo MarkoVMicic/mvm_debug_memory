49036fefa7f75509f3e9896d32f0d4d032566170
MarkoVMicic <marko.v.micic@gmail.com>
2022-12-20 00:11:00 +0800

    Update Turn on function to reflect new data structures and functions.
        In particular:
        1) Rewrote the initialization of the local DebugInfo variable to more
            adequately indicate that it is a pointer.
        2) Zero-initialize the entire function.
        3) Indicate that the Turn on function has no bytes allocated (duplicate
            work that probably will get optimized out by the compiler).
        4) Fix how the filenames are initialized and added by using the new
            functions.
        5) Add some error checking for all the arrays that are allocated.
    
    Parts 4 and 5 are duplicated later in other parts of the code.

--- mvm_debug_memory.h
+++ mvm_debug_memory.h
@@ -299,41 +299,80 @@ void MVMTurnOnDebugInfo(const char *Filename,
     }
 
     mvm_debug_memory_info *DebugInfo = 
-        &GlobalDebugInfoList->DebugInfoList[DebugInfoIndex];
+        GlobalDebugInfoList->DebugInfoList + DebugInfoIndex;
+
+    *DebugInfo = (mvm_debug_memory_info){0};
 
     DebugInfo->DebugInfoOpCount = 1;
+
+    // NOTE(Marko): This is not a memory allocation -- no bytes allocated. 
+    DebugInfo->ByteCountArrayCount = 0;
+    DebugInfo->ByteCountArrayAllocated = 0;
+    DebugInfo->ByteCountArray = 0;
+
     // NOTE(Marko): MVMTurnOnDebugInfo() can only ever be associated 
     //              with one filename and one line number!
     DebugInfo->FilenamesAllocated = 1; 
     DebugInfo->Filenames = 
-        (mvm_debug_memory_string *)malloc(
-            (sizeof DebugInfo->Filenames) * 
-            DebugInfo->FilenamesAllocated);
-    DebugInfo->Filenames[0] = 
-        ConstStringToMVMDebugMemoryString(Filename);
+        (mvm_debug_memory_string *)malloc((sizeof *DebugInfo->Filenames) *
+                                          DebugInfo->FilenamesAllocated);
+    if(DebugInfo->Filenames)
+    {        
+        DebugInfo->FilenamesCount = 1;
+        ZeroInitializeEmptyMVMDebugString(DebugInfo->Filenames);
+        AppendConstStringToMVMDebugMemoryString(Filename,
+                                                DebugInfo->Filenames);
+    }
+    else
+    {
+        printf("malloc() failed while allocating debug string structs\n");
+        DebugInfo->FilenamesAllocated = 0;
+        DebugInfo->FilenamesCount = 0;
+    }
 
     DebugInfo->LineNumbersAllocated = 1; 
     DebugInfo->LineNumbers = 
-        (int *)malloc((sizeof DebugInfo->LineNumbers) * 
+        (int *)malloc((sizeof *DebugInfo->LineNumbers) * 
                       DebugInfo->LineNumbersAllocated);
-    DebugInfo->LineNumbers[0] = LineNumber;
+    if(DebugInfo->LineNumbers)
+    {
+        DebugInfo->LineNumbers[0] = LineNumber;
+        DebugInfo->LineNumbersCount = 1;
+    }
+    else
+    {
+        printf("malloc() failed while allocating array for line numbers\n");
+        DebugInfo->LineNumbersAllocated = 0; 
+        DebugInfo->LineNumbersCount = 0; 
+    }
 
     DebugInfo->InitialAddress = 0;
     DebugInfo->CurrentAddress = 0;
     DebugInfo->PreviousAddress = 0;
-    DebugInfo->Freed = 0;
+    DebugInfo->Freed = FREED_NOT_APPLICABLE;
 
     // NOTE(Marko): TurnOn op only ever has one operation associated with it. 
     DebugInfo->MemoryOperationTypesAllocated = 1;
-    DebugInfo->MemoryOperationTypesCount = 1;
     DebugInfo->MemoryOperationTypes = 
-        (memory_operation_type *)malloc(sizeof DebugInfo->MemoryOperationTypes);
-    DebugInfo->MemoryOperationTypes[0] = MemoryOperationType_TurnOn;
+        (memory_operation_type *)malloc(sizeof *DebugInfo->MemoryOperationTypes);
+    if(DebugInfo->MemoryOperationTypes)
+    {
+        DebugInfo->MemoryOperationTypes[0] = MemoryOperationType_TurnOn;
+        DebugInfo->MemoryOperationTypesCount = 1;
+    }
+    else
+    {
+        printf("malloc() failed while allocating array for memory op types\n");
+        DebugInfo->MemoryOperationTypesAllocated = 0;
+        DebugInfo->MemoryOperationTypesCount = 0;
+    }
 
     DebugInfo->AddressesAllocated = 0;
     DebugInfo->AddressesCount = 0;
     DebugInfo->Addresses = 0;
 }
+
+
 
 void MVMTurnOffDebugInfo(const char *Filename,
                          int LineNumber)
